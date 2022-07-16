93b5a23b6207fc759ccea1ec7c34a6fbbd7a341b
Joyce <joyce.mbt@gmail.com>
2022-07-16 16:33:29 -0300

    guardando o value do input em uma constante

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import validator from './validator.js';
+const submit = document.querySelector("#send");
+submit.addEventListener("click", function(e) {
+  e.preventDefault();
+  const numero = document.querySelector("#input-card-number");
+  const value = numero.value;
+  console.log(value);
+});
 
 console.log(validator);
