f1f95200907fdb9cf2a2b05a746e2469552155e6
Joyce <joyce.mbt@gmail.com>
2022-07-16 19:23:52 -0300

    multipliquei por 2 os numeros da posiçao par do numero do cartao de credito

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ import validator from './validator.js';
 const submit = document.querySelector("#send");
 submit.addEventListener("click", function(e) {
   e.preventDefault();
-  const numero = document.querySelector("#input-card-number");
-  const value = numero.value;
-  console.log(value);
+  const number = document.querySelector("#input-card-number");
+  const value = number.value;
+  const listValue = value.split("");
+  const numberListValue = listValue.map(function(number) {
+    return parseInt(number);
+  });
+  const reverseList = numberListValue.reverse();
+  function isEven(index) {
+    if (index % 2 === 0) {
+      return true;
+    } 
+  };
+  const doubleNumber = reverseList.map(function(number) {
+    if (isEven(reverseList.indexOf(number)) === true) {
+      return number * 2;
+    } else {
+      return number;
+    }
+  });
+  console.log("list ao contrario em dobro", doubleNumber);
 });
 
 console.log(validator);
