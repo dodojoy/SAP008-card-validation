730577f3067a89bba89b1acb56445ea9c60bf533
Joyce <joyce.mbt@gmail.com>
2022-07-20 18:00:06 -0300

    Deixei no arquivo apenas a parte que diz respeito ao arquivo

--- index.js
+++ index.js
@@ -4,24 +4,6 @@ submit.addEventListener("click", function(e) {
   e.preventDefault();
   const number = document.querySelector("#input-card-number");
   const value = number.value;
-  const listValue = value.split("");
-  const numberListValue = listValue.map(function(number) {
-    return parseInt(number);
-  });
-  const reverseList = numberListValue.reverse();
-  function isEven(index) {
-    if (index % 2 === 0) {
-      return true;
-    } 
-  };
-  const doubleNumber = reverseList.map(function(number, index) {
-    if (isEven(index) === true) {
-      return number;
-    } else {
-      return number * 2;
-    }
-  });
-console.log("lista ao contrário dobrada " + doubleNumber);
+  console.log(validator.cardValidator(value));
+  console.log(validator.maskify(value));
 });
-
-console.log(validator);
